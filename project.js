c936b66f94067fe4a96aa0fa79822600940b3560
dammi-234 <mesegeya@gmail.com>
2025-06-15 19:13:00 +0000

    0616

--- project.js
+++ project.js
@@ -21,13 +21,98 @@ document.addEventListener('DOMContentLoaded', () => {
   let velocity = 0;
   let prevCenterIndex = -1;
 
-  // 카드 데이터 (15개)
-  const cardsData = Array.from({ length: TOTAL_CARDS }, (_, i) => ({
-    img: `project${i + 1}.jpg`,
-    titleKor: `한글 제목 ${i+1}`,
-    titleEng: `English Title ${i+1}`,
-    desc: `이것은 ${i+1}번째 카드의 설명입니다.`
-  }));
+const cardsData = [
+  {
+    img: 'project1.jpg',
+    titleKor: '타이포그래픽스',
+    titleEng: 'MEDIAVIDEO',
+    desc: '이 디자인은 타이포와 기하학을 이용한<br>모더니즘 느낌의 포스터입니다.'
+  },
+  {
+    img: 'project2.jpg',
+    titleKor: '광고미디어디자인',
+    titleEng: 'LEMOUTON',
+    desc: 'AI를 활용한 르무퉁 제품광고 포스터입니다.'
+  },
+  {
+    img: 'project3.jpg',
+    titleKor: '광고미디어디자인',
+    titleEng: '당신에게 테이핑을 더하다',
+    desc: '저를 광고하는 포스터로 좋은 기운을 드릴 수 있는 부적을<br>빗대어 표현하였습니다.'
+  },
+  {
+    img: 'project4.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: '학과 카드뉴스',
+    desc: '학과 카드뉴스로 제가 디자인한 학과 캐릭터 콘비 콘지를 넣었습니다.'
+  },
+  {
+    img: 'project5.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: '학과 카드뉴스',
+    desc: '학과 집부 카드뉴스로 게임 컨셉으로 디자인했습니다.'
+  },
+  {
+    img: 'project6.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: '학과 카드뉴스',
+    desc: '학과 카드뉴스를 시각적으로 디자인해보았습니다.'
+  },
+  {
+    img: 'project7.jpg',
+    titleKor: 'Illustration',
+    titleEng: '초코비',
+    desc: '짱구가 좋아하는 초코비를 방처럼 꾸며보았습니다.'
+  },
+  {
+    img: 'project8.jpg',
+    titleKor: 'CINEMA 4D',
+    titleEng: 'Magic House',
+    desc: '해리포터컨셉으로 제작한 마법사의 방을 상상해서 제작해보았습니다.'
+  },
+  {
+    img: 'project9.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: '학과 카드뉴스',
+    desc: '학과 카드뉴스로 시험을 응원하기 위해 제작했습니다.'
+  },
+  {
+    img: 'project10.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: 'MEDIACONTENTS',
+    desc: '학과 행사를 위해 제작했습니다.'
+  },
+  {
+    img: 'project11.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: '별무리 ADVENTURE',
+    desc: '대동제 부스 운영을 위해 제작한 카드뉴스'
+  },
+  {
+    img: 'project12.jpg',
+    titleKor: 'ILLUSTRATOR',
+    titleEng: 'PUBLIC TRANSPORTATION',
+    desc: '대중교통이라는 주제를 테트리스와 결합하여 제작한 디자인입니다.'
+  },
+  {
+    img: 'project13.jpg',
+    titleKor: 'ILLUSTRATOR',
+    titleEng: 'SMART PHONE',
+    desc: '스마트폰이라는 주제를 APPLE STORE 마트와 결합하여 제작했습니다.'
+  },
+  {
+    img: 'project14.jpg',
+    titleKor: 'CARDNEWS',
+    titleEng: '학과 카드뉴스',
+    desc: '학과 월간미콘입니다.'
+  },
+  {
+    img: 'project15.jpg',
+    titleKor: 'Application',
+    titleEng: 'APP DESIGN',
+    desc: '제가 좋아하는 드라마를 모아놓은 어플리케이션을 디자인해보았습니다.'
+  }
+];
 
   // 인디케이터 5개 생성
   for (let i = 0; i < TOTAL_INDICATORS; i++) {
